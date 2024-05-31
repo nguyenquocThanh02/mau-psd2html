8a6cd258e81026d91408f1329e201f76158ddf13
quocThanh <2002nguyenquocthanh@gmail.com>
2024-05-31 08:28:19 +0700

    thanh

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,7 +30,7 @@ module.exports = {
     semi: ["error", "always"],
     quotes: ["error", "double"],
     indent: ["error", 2],
-    "comma-dangle": ["error"],
+    "comma-dangle": ["error", "always"],
     "arrow-spacing": ["error"],
     "no-undef": ["error"],
     "no-unused-vars": ["error"],
