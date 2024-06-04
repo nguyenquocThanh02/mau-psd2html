6bf75d7524e2cbf04623d813c501fb1379fb05e5
quocThanh <2002nguyenquocthanh@gmail.com>
2024-06-04 08:38:48 +0700

    feat(test): test

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,6 @@ import "./App.css";
 import "./index.css";
 
 const App = () => {
-  // console.log(aB);
-
-  // if (a === b) {
-  //   console.log("ạdjdf");
-  // }
-
-  // for (x = 1; x <= 2; x++) {
-  //   let m = 1;
-  //   console.log(m);
-  // }
-
-  // console.log(m);
-
-  // const h = [a, c, b];
-
   return <div className="container"></div>;
 };
 
